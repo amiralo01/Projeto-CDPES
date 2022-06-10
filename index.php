1c1c5faecd52fc34c19d5b9f482b5e62aeb4fce3
Amiraldo Ferreira <102382923+amiralo01@users.noreply.github.com>
2022-06-10 15:52:29 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link real="stylesheet" type="text/CSS" href="./HTML/CSS/main.css">
+    <title>CDPES</title>
+</head>
+<body>
+    <?php
+    ?>
+    <header class="menu-principal">
+        <main>
+            <div class="header-1">
+                <div class="logo">
+                    <img src="./HTML/img/logo.png" />
+                </div>
+            </div>
+        </main>
+    </header>
+</body>
+</html>
