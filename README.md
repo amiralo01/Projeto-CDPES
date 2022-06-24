d0658d4f3be29948ae7ef429725e732ef33c235b
Amiraldo Ferreira <102382923+amiralo01@users.noreply.github.com>
2022-06-24 12:04:34 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # CDPES(Consultoria Digital)🤓
 ### Criação do site da CDPES, é uma Consultoria Digital para Pequenas Empresas de Santarém, também se trata do projeto que estou desenvolvendo tanto para o projeto de Empreendedorismo quanto para o projeto de Programação para Web.
 
+<h4 align="center">Em desenvolvimento</h4>
+
 ## Organização⚠️
 Modelo tendo como base a visão do cliente sobre o site, sendo um site que busque demonstrar tudo sobre a CDPES e como uma consultoria pode auxiliar nos negócios de uma empresa e demonstra quais o planos que a CDPES oferece.
 
@@ -8,6 +10,7 @@ Modelo tendo como base a visão do cliente sobre o site, sendo um site que busqu
 Necessário baixar e instalar os servidores web abaixo:
 - [XAMPP](https://www.apachefriends.org/pt_br/index.html)
 - [LAMP](https://www.edivaldobrito.com.br/instale-lamp-no-linux-e-tenha-um-servidor-web-em-seu-pc/)
+
 Execute o scrip database.sql localizado no diretório HTML para a sua execução da sua base de dados.
 
 ## Como Funciona🤝
@@ -41,11 +44,11 @@ Atualmente estou com alguns problemas para relacionar a parte de main.css com a 
 - Adicionar novas imagens tanto para ícone quanto para backgrounds.
 - Página de Contato completa e funcional que peça Nome, email, telefone e nome da empresa.
 - Página de Planos completa e funcional na qual o usuário poderá escolher o plano de negócios.
-- Página Inicial completa e funcional com paginações para serviços, portfólio e contato. Além de uma opção que leve para a página de planos, no caso um botão escrito "solicite o orçamento", onde logo acima deste botão também irá conter uma breve descrição da agência.
-- Página Contato completa e funcional onde o cliente poderá se registrar para que os administradores verfiquem ele no anco de dados e possam entrar em contato com o mesmo.
+- Página Inicial completa e funcional com paginações para serviços, portfólio e contato. Além de uma opção que leve para a página de planos, no caso um botão escrito "solicite o orçamento", onde logo acima deste botão também irá conter uma breve descrição da agência(este botão não foi implementado a página é basicamente um background).
+- Página Contato completa e funcional onde o cliente poderá se registrar para que os administradores verfiquem ele no banco de dados e possam entrar em contato com o mesmo(houve algum problema onde o banco de dados não registra os clientes).
 - Página de Portfolio completa e funcional onde o cliente poderá ver os trabalhos já feitos pela agência, através da imagens das empresas que ajudaram.
-- Criação de validadores em JavaScript para algumas páginas, de preferência para o cadastro em Contato.
-- Criação de um banco de dados em Sql inteligado ao PHP.
+- Criação de validadores em PHP e HTML para algumas páginas, de preferência para o cadastro em Contato.
+- Criação de um banco de dados em Sql inteligado ao PHP(database.sql não foi utilizado).
 
 ## Desenvolvimentos futuros📜
 + Criar mais opções e uma melhor estrutura para o guichê além adicionar o CSS com a estilização das páginas.
---
+++ contato.php
@@ -0,0 +1,94 @@
+<?php
+if (!isset($_SESSION)) {
+    session_start();
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&display=swap" rel="stylesheet">
+    
+    <link rel="stylesheet" href="./css/contato.css">
+    <title>CDPES</title>
+</head>
+
+<body>
+    <?php
+    if (isset($_SESSION['msg'])) {
+        echo "Bem vindo ", $_SESSION['msg'];
+        unset($_SESSION['msg']);
+    }
+    ?>
+    <header>
+        <div class="side-left">
+            <img class="logo" src="img/logo.png" alt="logo" width="60">
+        </div>
+
+        <div class="side-right">
+            <a href="index.php">Início</a>
+            <a href="servicos.php">Serviços</a>
+            <a href="portifolio.php">Portfólio</a>
+            <a href="contato.php">Contato</a>
+        </div>
+    </header>
+
+
+    <main>
+        <div class="menu-principal">
+
+            <div class="title">
+                O que é preciso para contratar nosso serviços?
+            </div>
+            <div class="container">
+                <div class="title-container">
+                    Basta preencher o formulário abaixo para que nossa equipe entre em contato com você:
+                </div>
+                <div id="main-container">
+                    <form id="form" method="get" onsubmit="alert('Formulário enviado com sucesso!')" action="process.php">
+                        <fieldset>
+
+                            <div class="label-float">
+                                <label id="labelNome"for="name">*Nome</label> 
+                                <br>
+                                <input type="text" id="name" name="nome" paceholder="" size="35" required autofocus/>
+                                <br>
+                            </div>
+                            <div class="label-float">
+                                <label id="labelEmail"for="email">*E-mail</label> 
+                                <br>
+                                <input type="email" name="email" id="email" size="25" paceholder="" required/>
+                                <br>
+                            </div>
+                            <div class="label-float">
+                                <label for="telefone">*Telefone</label>
+                                <br>
+                                <input type="tel" id="phone" size="35" paceholder="DD *****-****" pattern="[0-9]{2} [0-9]{5}-[0-9]{4}" required/>
+
+                            </div>
+                            <div class="label-float">
+                                <label id="name" for="empresa">*Empresa</label>
+                                <br>
+                                <input type="text" id="empresa" size="35" paceholder="" required autofocus/> 
+                                <br>
+                            </div>
+                            <div>
+                                <input type="checkbox" id="control" required/>
+                                <label for="agreement" id="agreement-label">Solicito atendimento</label>
+                                <br>
+                            </div>
+                            <input id="btn-submit" type="submit" value="Confirmar"/>
+
+                        </div>
+                        </fieldset>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </main>
+</body>
+
+</html>
