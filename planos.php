24c8b1b508003ed6b0bb9a8d91619b5d103c4599
Amiraldo Ferreira <102382923+amiralo01@users.noreply.github.com>
2022-07-14 00:14:06 -0300

    Add files via upload
    Implementação da página de planos(ainda falta algumas melhorias nela)

---
+++ planos.php
@@ -0,0 +1,120 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="keyworks" content="planos, cdpes, consultoria, quanto custa?">
+    <meta name="author" content="Amiraldo Filho">
+    <link rel="stylesheet" href="./css/planos.css">
+    <title>Planos</title>
+    
+</head>
+<body>
+    <header class="cabecalho">
+        <a>
+            <h1>PLANOS</h1>
+        </a>
+    </header>
+
+    <div class="containers">
+        <div class="container1">
+            <div class="title-container1">
+                Plano Básico
+            </div>
+            
+            <div class= "image1">
+                <img class="Básico" src="img/basico.png" alt="Básico" width="140">
+            </div>
+
+            <div class="Preço1">
+                R$ 1.100,00/mensal
+            </div>
+
+            <div class="content-container1">
+                <ul>
+                    <li>- Planejamento</li>
+                    <li>- Gestão de uma rede social</li>
+                    <li>- Conteúdo para redes sociais:</li>
+                    <li>  10 posts por mês</li>
+                    <li>- Gestão de Campanhas</li>
+                    <li>  Google Ads</li>
+                    <li>- 1 Reunião Online Mês</li>
+                </ul>
+            </div>
+
+            <div class="contratar">
+                <a href="http://localhost/CDPES/html/contato.php">CONTRATAR PLANO</a>
+            </div>
+        </div>
+
+        <div class="container2">
+            <div class="title-container2">
+                Plano Top
+            </div>
+            
+            <div class= "image2">
+                <img class="Top" src="img/top.png" alt="Top" width="140">
+            </div>
+
+            <div class="Preço2">
+                R$ 1.900,00/mensal
+            </div>
+
+            <div class="content-container2">
+                <ul>
+                    <li>- Planejamento</li>
+                    <li>- Criação de Site/Loja Virtual</li>
+                    <li>- Manutenção do Site</li>
+                    <li>- Gestão de 2 Redes Sociais</li>
+                    <li>- Conteúdo para redes sociais:</li>
+                    <li>  14 posts por mês</li>
+                    <li>- Gestão de Campanhas</li>
+                    <li>  Google Ads e Facebook Ads</li>
+                    <li>- 2 Reuniões Online Mês</li>
+                </ul>
+            </div>
+
+            <div class="contratar">
+                <a href="http://localhost/CDPES/html/contato.php">CONTRATAR PLANO</a>
+            </div>
+        </div>
+
+        <div class="container3">
+            <div class="title-container3">
+                Plano Premium
+            </div>
+            
+            <div class= "image3">
+                <img class="Premium" src="img/premium.png" alt="Premium" width="140">
+            </div>
+
+            <div class="Preço3">
+                R$ 3.400,00/mensal
+            </div>
+
+            <div class="content-container3">
+                <ul>
+                    <li>- Planejamento</li>
+                    <li>- Criação de Site/Loja Virtual</li>
+                    <li>- Manutenção do Site</li>
+                    <li>  Gestão de 3 Redes Sociais</li>
+                    <li>- Conteúso para Redes Sociais:</li>
+                    <li>  20 posts por mês</li>
+                    <li>- Gestão de Campanhas</li>
+                    <li>  Google Ads e Facebook Ads</li>
+                    <li>- 4 Reuniões Online Mês</li>
+                </ul>
+            </div>
+
+            <div class="contratar">
+                <a href="http://localhost/CDPES/html/contato.php">CONTRATAR PLANO</a>
+            </div>
+
+        </div>
+    </div>
+</body>
+</html>
